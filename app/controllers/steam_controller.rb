0c6be25300c237f5186cb2bf643b2443f61187e9
Kevin Sasse <sassek70@gmail.com>
2024-05-08 22:31:49 -0400

    tweaked endpoint

--- app/controllers/steam_controller.rb
+++ app/controllers/steam_controller.rb
@@ -9,8 +9,9 @@ class SteamController < ApplicationController
 
 
     def test_query
-        # url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_KEY']}&steamids=#{ENV['MY_ID']}"
-        url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_KEY']}&steamids=#{ENV['MY_ID']}/friends"
+        url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_KEY']}&steamids=#{ENV['MY_ID']}"
+        # url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_KEY']}&steamids=#{ENV['MY_ID']}/friends"
+        # url = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=#{ENV['STEAM_KEY']}&steamid=#{ENV['MY_ID']}&relationship=friend" -- gives error
         response = RestClient.get(url)
         parsed_response = JSON.parse(response)
         render json:{response: parsed_response}
